fc164e81acb25a5c3f1476fe465d340ab604fb94
yzy98 <yz186337@163.com>
2023-12-04 16:02:29 +0800

    Add: markeing page

---
+++ components/logo.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import Link from "next/link";
+import LocalFont from "next/font/local";
+
+import { cn } from "@/lib/utils";
+
+const headingFont = LocalFont({
+  src: "../public/fonts/font.woff2",
+});
+
+const Logo = () => {
+  return (
+    <Link href="/">
+      <div className="hover:opacity-75 transition items-center gap-x-2 hidden md:flex">
+        <Image src="/logo.svg" alt="Logo" height={30} width={30} />
+        <p
+          className={cn("text-lg text-neutral-700 pb-1", headingFont.className)}
+        >
+          Taskify
+        </p>
+      </div>
+    </Link>
+  );
+};
+
+export default Logo;
