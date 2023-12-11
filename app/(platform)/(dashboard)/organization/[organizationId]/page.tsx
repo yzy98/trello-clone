4fd39c15fee7f53716a128227ecdd1059f0af230
yzy98 <yz186337@163.com>
2023-12-11 20:26:36 +0800

    Add: organization page Info component

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,19 +1,11 @@
-import Board from "./board";
-import Form from "./form";
-
-import { db } from "@/lib/db";
+import { Separator } from "@/components/ui/separator";
+import Info from "./_components/info";
 
 const OrganizationIdPage = async () => {
-  const boards = await db.board.findMany();
-
   return (
-    <div className="flex flex-col space-y-4">
-      <Form />
-      <div className="space-y-2">
-        {boards.map((board) => (
-          <Board key={board.id} id={board.id} title={board.title} />
-        ))}
-      </div>
+    <div className="w-full mb-20">
+      <Info />
+      <Separator className="my-4" />
     </div>
   );
 };
