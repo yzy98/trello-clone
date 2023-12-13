91d15b0652bc50208fcec366997e01df13f5350a
yzy98 <yz186337@163.com>
2023-12-13 16:12:26 +0800

    Add: Board update and delete feature

---
+++ actions/delete-board/index.ts
@@ -0,0 +1,41 @@
+"use server";
+
+import { InputType, ReturnType } from "./types";
+import { DeleteBoard } from "./schema";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+import { auth } from "@clerk/nextjs";
+import { db } from "@/lib/db";
+import { createSafeAction } from "@/lib/create-safe-action";
+
+const handler = async (data: InputType): Promise<ReturnType> => {
+  const { userId, orgId } = auth();
+
+  if (!userId || !orgId) {
+    return {
+      error: "Unauthorized",
+    };
+  }
+
+  const { id } = data;
+  let board;
+
+  try {
+    board = await db.board.delete({
+      where: {
+        id,
+        orgId,
+      },
+    });
+  } catch (error) {
+    return {
+      error: "Failed to delete",
+    };
+  }
+
+  revalidatePath(`/organization/${orgId}`);
+  redirect(`/organization/${orgId}`);
+};
+
+export const deleteBoard = createSafeAction(DeleteBoard, handler);
--- app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-navbar.tsx
@@ -1,3 +1,4 @@
+import BoardOptions from "./board-options";
 import BoardTitleForm from "./board-title-form";
 
 import { Board } from "@prisma/client";
@@ -10,6 +11,9 @@ const BoardNavbar = ({ data }: BoardNavbarProps) => {
   return (
     <div className="w-full h-14 z-[40] bg-black/50 fixed top-14 flex items-center px-6 gap-x-4 text-white">
       <BoardTitleForm data={data} />
+      <div className="ml-auto">
+        <BoardOptions id={data.id} />
+      </div>
     </div>
   );
 };
