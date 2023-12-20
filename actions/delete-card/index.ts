27d01a7eed27897aebb3290b1f5b1f55c9fec7ca
yzy98 <yz186337@163.com>
2023-12-20 16:18:38 +0800

    Add: Card description & actions

---
+++ actions/delete-card/index.ts
@@ -0,0 +1,44 @@
+"use server";
+
+import { InputType, ReturnType } from "./types";
+import { DeleteCard } from "./schema";
+import { revalidatePath } from "next/cache";
+
+import { auth } from "@clerk/nextjs";
+import { db } from "@/lib/db";
+import { createSafeAction } from "@/lib/create-safe-action";
+
+const handler = async (data: InputType): Promise<ReturnType> => {
+  const { userId, orgId } = auth();
+
+  if (!userId || !orgId) {
+    return {
+      error: "Unauthorized",
+    };
+  }
+
+  const { id, boardId } = data;
+  let card;
+
+  try {
+    card = await db.card.delete({
+      where: {
+        id,
+        list: {
+          board: {
+            orgId,
+          },
+        },
+      },
+    });
+  } catch (error) {
+    return {
+      error: "Failed to delete",
+    };
+  }
+
+  revalidatePath(`/board/${boardId}`);
+  return { data: card };
+};
+
+export const deleteCard = createSafeAction(DeleteCard, handler);
